632b38ca237055d71c1e03f6b2bfd6dd1ca2036e
jitae <jitaekim93@gmail.com>
2018-11-30 20:04:20 +0900

    implement legendArcana function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,11 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 // const config = {
 //     apiKey: "AIzaSyBD9eX7ABB0Xu1N6CnSdKL-bnsNF5WgLtc",
-//     authDomain: "chainchronicle-ea233.firebaseapp.com",
+//     // authDomain: "chainchronicle-ea233.firebaseapp.com",
 //     databaseURL: "https://chainchronicle-ea233.firebaseio.com",
 //     projectId: "chainchronicle-ea233",
-//     storageBucket: "chainchronicle-ea233.appspot.com",
-//     messagingSenderId: "1008757887866"
+//     // storageBucket: "chainchronicle-ea233.appspot.com",
+//     // messagingSenderId: "1008757887866"
 // };
 const config = {
     apiKey: "AIzaSyBzl0p1jpmVi5sf8lywgJNCTWex0lAQTmw",
@@ -38,6 +38,23 @@ exports.festivalArcana = functions.https.onCall(async() => {
 
     try {
         const ref = db.collection('arcana').where('isFestival', '>=', 0);
+        const snapshot = await ref.get();
+        const array = [];
+        snapshot.forEach(doc => {
+            array.push(doc.data());
+        });
+        return array;
+    }
+    catch(error) {
+        return error;
+    }
+
+});
+
+exports.legendArcana = functions.https.onCall(async() => {
+
+    try {
+        const ref = db.collection('arcana').where('isLegend', '==', true);
         const snapshot = await ref.get();
         const array = [];
         snapshot.forEach(doc => {
